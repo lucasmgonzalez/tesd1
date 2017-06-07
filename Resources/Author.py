3ea38c1ed9ccce250ec8565533cca87254e0c67d
Luke Gonzalez <lucasmgonzalez1992@gmail.com>
2017-06-07 12:58:36 -0300

    Colocando mais coisa no gitignore e removendo o byte code do python

--- Resources/Author.py
+++ Resources/Author.py
@@ -1,9 +1,22 @@
 import MySQLdb
 
 class Resource:
+    def __init__(self):
+        self.db = MySQLdb.connect('localhost', 'user', '', 'tesd1')
+        self.cursor = self.db.cursor()
 
     def index(self):
-        return 'all authors'
+        sql = "SELECT * FROM authors"
+        self.cursor.execute(sql)
+
+        authors = self.cursor.fetchall()
+
+        return authors
 
     def get(self,id):
-        return id
+        sql = "SELECT * FROM authors WHERE id = '%d'" % (id)
+        self.cursor.execute(sql)
+
+        author = self.cursor.fetchone()
+
+        return author
