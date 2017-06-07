6085a0e2647a368a00367ce79ce4c7cc2b1df3ff
Luke Gonzalez <lucasmgonzalez1992@gmail.com>
2017-06-07 20:57:13 -0300

    Tentando fazer um resource - funciona o retrieve

--- Resources/Author.py
+++ Resources/Author.py
@@ -1,8 +1,9 @@
+from flask import abort,request
 import MySQLdb
 
 class Resource:
     def __init__(self):
-        self.db = MySQLdb.connect('localhost', 'user', '', 'tesd1')
+        self.db = MySQLdb.connect('192.168.10.10', 'homestead', 'secret', 'tesd1')
         self.cursor = self.db.cursor()
 
     def index(self):
@@ -13,10 +14,55 @@ class Resource:
 
         return authors
 
-    def get(self,id):
-        sql = "SELECT * FROM authors WHERE id = '%d'" % (id)
+    def retrieve(self, id):
+        sql = "SELECT * FROM authors WHERE id = '%d'" % id
+
         self.cursor.execute(sql)
 
         author = self.cursor.fetchone()
 
         return author
+
+    def store(self, request):
+
+        # if not request.json or not 'name' in request.json or not 'citation_name' in request.json or not 'cpf' in request.json:
+        #     abort(400)
+
+        return len(request)
+
+        # name = request.json['name']
+        # citation_name = request.json['citation_name']
+        # cpf = request.json['cpf']
+        #
+        # sql = "INSERT INTO authors (name, citation_name, cpf) VALUES(%s, %s, %s)" % (name, citation_name, cpf)
+        #
+        # try:
+        #     self.cursor.execute(sql)
+        #
+        #     self.db.commit()
+        # except:
+        #     self.db.rollback()
+        #
+        # return True
+
+    def update(self, id, name, citation_name, cpf):
+        sql = "UPDATE authors SET name = %s, citation_name = %s, cpf = %s WHERE id = %d" % (name, citation_name, cpf, id)
+
+        try:
+            self.cursor.execute(sql)
+
+            self.db.commit()
+        except:
+            self.db.rollback()
+
+        sql = "SELECT * FROM authors WHERE id = %d" % (id)
+
+        self.cursor.execute(sql)
+
+        author = self.cursor.fetchone()
+
+        return author
+
+    def delete(self, id):
+
+        return True
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 from Resources import Author
 
 app = Flask(__name__)
 
 author_resource = Author.Resource()
 
+
 @app.route('/')
 def index():
     return 'Hello World!'
+
 
 @app.route('/authors', methods=['GET'])
 def get_authors():
     return jsonify(author_resource.index())
 
+
+@app.route('/author', methods=['POST'])
+def create_author():
+    return jsonify(author_resource.store(request.json))
+
+
 @app.route('/author/<int:id>', methods=['GET'])
-def get_author(id):
-    return jsonify(author_resource.get(id))
+def retrieve_author(id):
+    return jsonify(author_resource.retrieve(id))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
